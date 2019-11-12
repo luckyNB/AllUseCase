0301d255a8e77d2870116132113fdc9e07a3f937
luckyNB <20150773@dbatu.ac.in>
2019-11-12 19:15:45 +0530

    Function is created and Counted the number of maximum count of coin

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,107 +1,43 @@
 #!/bin/bash -x
-	counter=0
-	headTripplet=0
-	tailTripplet=0
-	H=0
-	T=0
-        HH=0
-        HT=0
-	TT=0
-	TH=0
-        HHH=0
-        HHT=0
-        HTH=0
-        THH=0
-        TTT=0
-        TTH=0
-        THT=0
-        HTT=0
-	#function singletCount(){
-	#echo 
-	#}
-	#function doubletCount(){
-	#}
-	#function tripletCount(){
-	#}
-
-        
-	declare -A CoinDictionary
-	read repetition
-
-rep=$repetition
-
-while [ $rep -gt 0 ]
-			do
-			event=$((RANDOM%2))	
-			if [ $event -eq 0 ]
-				then
-				CoinDictionary[$((counter++))]="H"
-                                H=$(($H + 1))
-  		       else
-				CoinDictionary[$((counter++))]="T"
-                                T=$(($T + 1))
-        		fi
-			rep=$(($rep - 1))
-		done
-
-
-
-
-	while [ $repetition -gt 0 ]
-			do
-			event=$((RANDOM%8))
-  case $event in 
-      0)
-		CoinDictionary[$((counter++))]="HHH"
-              HHH=$(($HHH + 1));;
-    1)
-		CoinDictionary[$((counter++))]="HHT"
-      	      HHT=$(($HHT + 1));;
-    2)
-		CoinDictionary[$((counter++))]="HTH"
-	      HTH=$(($HTH + 1))       	 
-       ;;
-      
-    3)
-		CoinDictionary[$((counter++))]="THH"
-	      THH=$(($THH + 1)) 
-      		;;
-    4)
-		CoinDictionary[$((counter++))]="TTT"
-    	      TTT=$(($TTT + 1));;    
-    5)   
-	  	CoinDictionary[$((counter++))]="TTH"
-    	      TTT=$(($TTT + 1))    
-       		;;    
-    6)  
-        	CoinDictionary[$((counter++))]="THT"
-    	      TTH=$(($TTH + 1))    
-      		;;    
-    7)
-		CoinDictionary[$((counter++))]="HTT"
-    	      HTT=$(($HTT + 1))    
-     		;;     
 	
-	esac
+	read -p "How much time do you want to flip" numberOfTimes
+ 
+	read -p "How many coin do you want to flip" coins
+declare -A coinDictionary
 
-		repetition=$(($repetition -1))
-	done	
+	headCount=0
+	tailCount=0
+	tailTail=0
+	headHead=0
+	tailHead=0
+	HeadTail=0
 
-finalArrayOfCoins[1]=$H;
-finalArrayOfCoins[2]=$T;
-finalArrayOfCoins[3]=$HHH;
-finalArrayOfCoins[4]=$HHT;
-finalArrayOfCoins[5]=$HTH;
-finalArrayOfCoins[6]=$THH;
-finalArrayOfCoins[7]=$TTT;
-finalArrayOfCoins[8]=$TTH;
-finalArrayOfCoins[9]=$THT;
-finalArrayOfCoins[10]=$HTT;
+	function flipCoin() {
+	for (( i=1; i<=$numberOfTimes; i++ ))
+		do
+		s=""
+ 		 for(( j=0; j<$coins; j++ ))
+ 			do
+    			resultk=$(( RANDOM % 2 ))
 
-echo $(printf "%s\n"${!finalArrayOfCoins[@]} | sort -n)
+     			 if [ $resultk == 1 ]
+				then
+   				s=$s"H"
+                          headCount=$(( $headCount + 1 ))
+                        else
+                                s=$s"T"
+                          tailCount=$(( $tailCount + 1))
+                        fi
+                        done
+             echo final key : $s
+             coinDictionary["$s"]=$(( ${coinDictionary["$s"]} + 1 ))   
 
+              done
+}
+flipCoin
+echo ${coinDictionary[@]}
 
-
+echo ${!coinDictionary[@]}
 
 	
 	
