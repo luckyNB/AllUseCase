8808f49ff93d1aeb6bd1f3f52bc7cf79ac471088
luckyNB <20150773@dbatu.ac.in>
2019-11-12 12:51:01 +0530

    After calculating the percentage of head and  tail  the program will be

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,9 +1,34 @@
 #!/bin/bash -x
-event=$((RANDOM%2))
-	if [ $event -eq 0 ]
-		then
-  	echo "Head"
-         else
-             echo "Tail"
-        fi
+
+	counter=0
+	head=0
+	tail=0
+	declare -A CoinDictionary
+	read repetition
+		while [ $repetition -gt 0 ]
+			do
+			event=$((RANDOM%2))	
+			if [ $event -eq 0 ]
+				then
+				CoinDictionary[$((counter++))]="Head"
+                                head=$(($head + 1))
+  		       else
+				CoinDictionary[$((counter++))]="Tail"
+                                tail=$(($tail + 1))
+        		fi
+			repetition=$(($repetition - 1))
+		done
+	echo "All events" ${CoinDictionary[@]}
+
+	counter=0
+  	  for key in ${!CoinDictionary[@]}
+			do
+				CoinArray[((counter++))]=${CoinDictionary[$key]}
+			done
+
+events=${#CoinDictionary[@]}
+echo "Number of events" ${#CoinDictionary[@]}
+echo "Array" ${CoinArray[@]}
+echo "Head Percentage" percentage=$(($head*100/$events))
+echo "Tail Percentage" percentage=$(($tail*100/$events))
 
