3842c8079b3e97a7d2129327cecf4bc4f0082651
luckyNB <20150773@dbatu.ac.in>
2019-11-12 17:18:29 +0530

    Sorting of singlet,duplet and tripplet are stored in dictionary and sorted and found largest count

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,38 +1,85 @@
 #!/bin/bash -x
-
 	counter=0
 	headTripplet=0
 	tailTripplet=0
+	H=0
+	T=0
+        HH=0
+        HT=0
+	TT=0
+	TH=0
+        HHH=0
+        HHT=0
+        HTH=0
+        THH=0
+        TTT=0
+        TTH=0
+        THT=0
+        HTT=0
+	#function singletCount(){
+	#echo 
+	#}
+	#function doubletCount(){
+	#}
+	#function tripletCount(){
+	#}
+
+        
 	declare -A CoinDictionary
 	read repetition
+
+rep=$repetition
+
+while [ $rep -gt 0 ]
+			do
+			event=$((RANDOM%2))	
+			if [ $event -eq 0 ]
+				then
+				CoinDictionary[$((counter++))]="H"
+                                H=$(($H + 1))
+  		       else
+				CoinDictionary[$((counter++))]="T"
+                                T=$(($T + 1))
+        		fi
+			rep=$(($rep - 1))
+		done
+
+
+
+
 	while [ $repetition -gt 0 ]
 			do
 			event=$((RANDOM%8))
   case $event in 
       0)
 		CoinDictionary[$((counter++))]="HHH"
-        	headTripplet=$(($headTripplet + 1));;
-     1)
-		CoinDictionary[$((counter++))]="HHT";;
-      
-     2)
+              HHH=$(($HHH + 1));;
+    1)
+		CoinDictionary[$((counter++))]="HHT"
+      	      HHT=$(($HHT + 1));;
+    2)
 		CoinDictionary[$((counter++))]="HTH"
-       	        ;;
+	      HTH=$(($HTH + 1))       	 
+       ;;
       
-     3)
+    3)
 		CoinDictionary[$((counter++))]="THH"
+	      THH=$(($THH + 1)) 
       		;;
     4)
 		CoinDictionary[$((counter++))]="TTT"
-    	      tailTripplet=$(($tailTripplet + 1));;    
+    	      TTT=$(($TTT + 1));;    
     5)   
 	  	CoinDictionary[$((counter++))]="TTH"
+    	      TTT=$(($TTT + 1))    
        		;;    
-   6)  
+    6)  
         	CoinDictionary[$((counter++))]="THT"
+    	      TTH=$(($TTH + 1))    
       		;;    
-   7)
+    7)
 		CoinDictionary[$((counter++))]="HTT"
+    	      HTT=$(($HTT + 1))    
      		;;     
 	
 	esac
@@ -40,18 +87,21 @@
 		repetition=$(($repetition -1))
 	done	
 
-		echo "All events" ${CoinDictionary[@]}
+finalArrayOfCoins[1]=$H;
+finalArrayOfCoins[2]=$T;
+finalArrayOfCoins[3]=$HHH;
+finalArrayOfCoins[4]=$HHT;
+finalArrayOfCoins[5]=$HTH;
+finalArrayOfCoins[6]=$THH;
+finalArrayOfCoins[7]=$TTT;
+finalArrayOfCoins[8]=$TTH;
+finalArrayOfCoins[9]=$THT;
+finalArrayOfCoins[10]=$HTT;
 
-		counter=0
-  	  for key in ${!CoinDictionary[@]}
-			do
-				CoinArray[((counter++))]=${CoinDictionary[$key]}
-			done
-
-	events=${#CoinDictionary[@]}
-	echo "Number of events" ${#CoinDictionary[@]}
-	echo "Array" ${CoinArray[@]}
-echo "Head Doulbet Percentage" percentage=$(($headTripplet*100/$events))
-echo "Tail Doublet Percentage" percentage=$(($tailTripplet*100/$events))
+echo $(printf "%s\n"${!finalArrayOfCoins[@]} | sort -n)
 
 
+
+
+	
+	
