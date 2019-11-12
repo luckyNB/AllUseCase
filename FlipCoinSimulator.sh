6b7780a7b62c00a42a697df03cf0559ae41fc35c
luckyNB <20150773@dbatu.ac.in>
2019-11-12 15:35:28 +0530

    After complting Duplet used swich case and made it as Tripplet for use case-4

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,45 +1,57 @@
 #!/bin/bash -x
 
 	counter=0
-	headDoublet=0
-	tailDoublet=0
+	headTripplet=0
+	tailTripplet=0
 	declare -A CoinDictionary
 	read repetition
-		while [ $repetition -gt 0 ]
+	while [ $repetition -gt 0 ]
 			do
-			event=$((RANDOM%4))	
-			if [ $event -eq 0 ]
-				then
-				CoinDictionary[$((counter++))]="HH"
-                                headDoublet=$(($headDoublet + 1))
+			event=$((RANDOM%8))
+  case $event in 
+      0)
+		CoinDictionary[$((counter++))]="HHH"
+        	headTripplet=$(($headTripplet + 1));;
+     1)
+		CoinDictionary[$((counter++))]="HHT";;
+      
+     2)
+		CoinDictionary[$((counter++))]="HTH"
+       	        ;;
+      
+     3)
+		CoinDictionary[$((counter++))]="THH"
+      		;;
+    4)
+		CoinDictionary[$((counter++))]="TTT"
+    	      tailTripplet=$(($tailTripplet + 1));;    
+    5)   
+	  	CoinDictionary[$((counter++))]="TTH"
+       		;;    
+   6)  
+        	CoinDictionary[$((counter++))]="THT"
+      		;;    
+   7)
+		CoinDictionary[$((counter++))]="HTT"
+     		;;     
+	
+	esac
 
-			elif [ $event -eq 1 ]
-				then
-				CoinDictionary[$((counter++))]="HT"
+		repetition=$(($repetition -1))
+	done	
 
+		echo "All events" ${CoinDictionary[@]}
 
-			elif [ $event -eq 2 ]
-				then
-				CoinDictionary[$((counter++))]="TH"
-
-
-
-  		       else [ $event -eq 3 ]
-				CoinDictionary[$((counter++))]="TT"
-                                tailDoublet=$(($tailDoublet + 1))
-        		fi
-			repetition=$(($repetition - 1))
-		done
-	echo "All events" ${CoinDictionary[@]}
-
-	counter=0
+		counter=0
   	  for key in ${!CoinDictionary[@]}
 			do
 				CoinArray[((counter++))]=${CoinDictionary[$key]}
 			done
 
-events=${#CoinDictionary[@]}
-echo "Number of events" ${#CoinDictionary[@]}
-echo "Array" ${CoinArray[@]}
-echo "Head Doulbet Percentage" percentage=$(($headDoublet*100/$events))
-echo "Tail Doublet Percentage" percentage=$(($tailDoublet*100/$events))
+	events=${#CoinDictionary[@]}
+	echo "Number of events" ${#CoinDictionary[@]}
+	echo "Array" ${CoinArray[@]}
+echo "Head Doulbet Percentage" percentage=$(($headTripplet*100/$events))
+echo "Tail Doublet Percentage" percentage=$(($tailTripplet*100/$events))
+
+
